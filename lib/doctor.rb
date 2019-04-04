3ebebd307cde5d5dbd612fa9891dfb02ab8c65b2
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-04 20:37:05 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,4 +19,20 @@ class Doctor
     end
     resulting_array
   end
+  def patients
+    appointments_array = self.appointments
+    solution_array = []
+    appointments_array.each do |appointment|
+      unique_condition = true
+      solution_array.each do |patient|
+        if appointment.patient == patient
+          unique_condition = false
+        end
+      end
+      if unique_condition == true
+        solution_array << appointment.patient
+      end
+    end
+    solution_array
+  end
 end
