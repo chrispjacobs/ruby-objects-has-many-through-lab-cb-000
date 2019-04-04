546828be6cffab3022cf098178ea817938acd658
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-04 20:29:47 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,8 @@
+class Appointment
+  @@all = []
+  attr_accessor :patient, :doctor, :date
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+end
