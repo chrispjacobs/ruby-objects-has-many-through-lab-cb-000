0cd05722ea0e385369e665681439ebce82c0b1c8
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-04 20:02:34 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -10,7 +10,9 @@ sleater_kinney = Artist.new("Sleater Kinney")
 meshuggah.new_song("Ivory Tower", heavy_metal)
 meshuggah.new_song("Koloss", heavy_metal)
 system_of_a_down.new_song("Bounce!", heavy_metal)
-puts "system_of_a_down.songs returns #{system_of_a_down.songs}"
-puts "meshuggah.songs returns #{meshuggah.songs}"
-sleater_kinney.new_song("Dig Me Out", punk)
-puts "heavy_metal.artists returns #{heavy_metal.artists}"
+# puts "system_of_a_down.songs returns #{system_of_a_down.songs}"
+# puts "meshuggah.songs returns #{meshuggah.songs}"
+# sleater_kinney.new_song("Dig Me Out", punk)
+# puts "heavy_metal.artists returns #{heavy_metal.artists}"
+obzen = Song.new("Obzen", meshuggah, heavy_metal)
+puts "obzen.genre returns #{obzen.genre}"
