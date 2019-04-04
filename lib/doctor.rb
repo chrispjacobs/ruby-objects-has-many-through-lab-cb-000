26ad194433f378d37c7d3132fc109e4aa4ee39fa
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-04 20:33:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,22 @@
 class Doctor
   @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  attr_accessor :name
   def new_appointment(patient_object, date)
     new_appt = appointment.new(patient_object, self, date)
     new_appt.doctor = self
     new_appt
+  end
+  def appointments
+    resulting_array = []
+    Appointment.all do |appointment|
+      if appointment.doctor == self
+        resulting_array << appointment
+      end
+    end
+    resulting_array
   end
 end
